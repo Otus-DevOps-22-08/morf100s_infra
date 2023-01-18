0934cb6538eb6a86bfb5c949943518a6aa25801a
Ilya Nedosvyaty <ilya.nedosvyaty@gralinte.com>
2023-01-18 20:03:59 +0300

    09 - HW - Принципы организации инфраструктурного кода и работа над инфраструктурой в команде на примере Terraform, правки после тестов

---
+++ terraform/modules/vpc/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+      version = "0.84.0"
+    }
+  }
+}
+
+resource "yandex_vpc_network" "app-network" {
+  name = "app-network"
+}
+
+resource "yandex_vpc_subnet" "app-subnet" {
+  name           = "app-subnet"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.app-network.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -1,12 +1,3 @@
-#terraform {
-#  required_providers {
-#    yandex = {
-#      source  = "yandex-cloud/yandex"
-#      version = "0.84.0"
-#    }
-#  }
-#}
-
 provider "yandex" {
   service_account_key_file = var.service_account_key_file
   cloud_id                 = var.cloud_id
@@ -14,16 +5,9 @@ provider "yandex" {
   zone                     = var.zone
 }
 
-#resource "yandex_vpc_network" "app-network" {
-#  name = "reddit-app-network"
-#}
-#
-#resource "yandex_vpc_subnet" "app-subnet" {
-#  name           = "reddit-app-subnet"
-#  zone           = "ru-central1-a"
-#  network_id     = yandex_vpc_network.app-network.id
-#  v4_cidr_blocks = ["192.168.10.0/24"]
-#}
+module "vpc" {
+  source = "../modules/vpc"
+}
 
 module "app" {
   source          = "../modules/app"
